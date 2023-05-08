e2a4fd3d823b601d09d4676331e3bf99f4ed3604
wiltondev <92389600+wiltondev@users.noreply.github.com>
2023-05-08 00:43:27 -0300

    upload imagens

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -1,47 +1,58 @@
-import type {NextApiRequest, NextApiResponse} from 'next';
-import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
-import type {cadastroRequisicao} from '../../types/cadastroRequisicao';
-import {UsuarioModel} from '../../models/UsuarioModel';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
+import type { cadastroRequisicao } from '../../types/cadastroRequisicao';
+import { UsuarioModel } from '../../models/UsuarioModel';
 import md5 from "md5";
-import {conectarMongoDB} from "../../middlewares/conectarMongoDB";
+import { conectarMongoDB } from "../../middlewares/conectarMongoDB";
+import { upload, uploadImagemCosmic } from "../../services/uploadImagemCosmic";
+import nextConnect from 'next-connect';
 
-const endpointCadastro =
-    async(req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) =>{
-    
-        if(req.method === 'POST'){
-            const usuario = req.body as cadastroRequisicao;
-            if(!usuario.nome || usuario.nome.length < 2){
-                return res.status(400).json({erro :'Nome invalido!'})
-            }
 
-            if(!usuario.email ||usuario.email.length < 5 
-                || !usuario.email.includes('@')
-                || !usuario.email.includes('.')){
-                return res.status(400).json({erro :'Nome invalido!'});
-            }
+const handler = nextConnect()
+    .use(upload.single('file'))
+    .post(async (req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>) => {
 
-            if(!usuario.senha ||usuario.senha.length < 4){
-                return res.status(400).json({erro :'Senha invalida!'})
-            }
-            // Validação se já existe usuario com mesmo email 
 
-            const usuariosComMesmoEmail = await UsuarioModel.find({email : usuario.email});
-            if(usuariosComMesmoEmail && usuariosComMesmoEmail.length >0 ){
-                return res.status(400).json({erro : 'já existe usuario com esse email'})
-            }
-
-            //salvar no banco de dados
-            const usuarioASerSalvo = {
-                nome : usuario.nome,
-                email : usuario.email,
-                senha : md5(usuario.senha)
-            }
-           await UsuarioModel.create(usuarioASerSalvo)
-            return res.status(200).json({msg: 'Usuario criado com sucesso!'});
-
+        const usuario = req.body as cadastroRequisicao;
+        if (!usuario.nome || usuario.nome.length < 2) {
+            return res.status(400).json({ erro: 'Nome invalido!' })
         }
-        return res.status(405).json({ erro : 'Método informado não é valido'});
 
- }
+        if (!usuario.email || usuario.email.length < 5
+            || !usuario.email.includes('@')
+            || !usuario.email.includes('.')) {
+            return res.status(400).json({ erro: 'Nome invalido!' });
+        }
 
- export default conectarMongoDB(endpointCadastro);
+        if (!usuario.senha || usuario.senha.length < 4) {
+            return res.status(400).json({ erro: 'Senha invalida!' })
+        }
+        // Validação se já existe usuario com mesmo email 
+
+        const usuariosComMesmoEmail = await UsuarioModel.find({ email: usuario.email });
+        if (usuariosComMesmoEmail && usuariosComMesmoEmail.length > 0) {
+            return res.status(400).json({ erro: 'já existe usuario com esse email' })
+        }
+
+        //enviar imagem do multer para o cosmic 
+
+        const image = await uploadImagemCosmic(req);
+
+        //salvar no banco de dados
+        const usuarioASerSalvo = {
+            nome: usuario.nome,
+            email: usuario.email,
+            senha: md5(usuario.senha),
+            avatar : image?.media?.url
+        }
+        await UsuarioModel.create(usuarioASerSalvo)
+        return res.status(200).json({ msg: 'Usuario criado com sucesso!' });
+
+    });
+        
+export const config ={
+    api:{
+        bodyParser : false
+    }
+}
+ export default conectarMongoDB(handler);
