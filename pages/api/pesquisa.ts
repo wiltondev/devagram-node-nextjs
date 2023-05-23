c522942de0d3662c00de47d9949fe2b1dbfa8455
wiltondev <92389600+wiltondev@users.noreply.github.com>
2023-05-22 22:41:00 -0300

    api like/deslike

--- pages/api/pesquisa.ts
+++ pages/api/pesquisa.ts
@@ -10,19 +10,33 @@ async (req: NextApiRequest, res : NextApiResponse<RespostaPadraoMsg| any[]>)=>{
     try {
 
         if(req.method ==='GET'){
-            const {filtro}= req.query;
-            if(!filtro || filtro.length < 2){
-                return res.status(400).json({erro: 'Favor informar pelo menos dois caracteres para a busca '})
+            if(req?.query?.id){
+                const usuarioEncontrado = await UsuarioModel.findById(req?.query?.id)
+                if(!usuarioEncontrado){
+                    return res.status(400).json({erro: 'Usuario não encontrado '})
+                }
+                usuarioEncontrado.senha = null
+                return res.status(200).json(usuarioEncontrado)
+
+            }else{
+                const {filtro}= req.query;
+                if(!filtro || filtro.length < 2){
+                    return res.status(400).json({erro: 'Favor informar pelo menos dois caracteres para a busca '})
+                }
+                const usuarioEncontrado  = await UsuarioModel.find({
+                    $or:[{ nome:{$regex: filtro, $options:'i'}},
+                        { email:{$regex: filtro, $options:'i'}},
+                    ]
+                          
+                });
+    
+                return res.status(200).json(usuarioEncontrado)
+    
             }
-            const usuarioEncontrado  = await UsuarioModel.find({
-                $or:[{ nome:{$regex: filtro, $options:'i'}},
-                    { email:{$regex: filtro, $options:'i'}},]
-                      
-               
-            });
 
-            return res.status(200).json(usuarioEncontrado)
 
+
+           
 
         }
         
