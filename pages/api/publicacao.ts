3d34df496a75fef392810a2add64469a77676efe
wiltondev <92389600+wiltondev@users.noreply.github.com>
2023-05-30 21:15:43 -0300

    seguir/desseguir

--- pages/api/publicacao.ts
+++ pages/api/publicacao.ts
@@ -11,56 +11,51 @@ import { UsuarioModel } from '../../models/UsuarioModel';
 
 const handler = nc()
     .use(upload.single('file'))
-    .post(async (req: any, res: NextApiResponse<RespostaPadraoMsg>) => {
-
-        try {
-            
-            const { userId } = req.query;
+    .post(async (req : any, res : NextApiResponse<RespostaPadraoMsg>) => {
+        try{
+            const {userId} = req.query;
+            console.log('userId:', userId);
             const usuario = await UsuarioModel.findById(userId);
-            if (!usuario) {
-                return res.status(400).json({ erro: 'Usuario não encontrado' });
+            if(!usuario){
+                return res.status(400).json({erro : 'Usuario nao encontrado'});
+            }
 
-            };
-           
-            if (!req || !req.body) {
-                return res.status(400).json({ erro: 'Parâmetros de entrada não informados!' });
-            };
+            if(!req || !req.body){
+                return res.status(400).json({erro : 'Parâmetros de entrada nao informados'});
+            }
+            const {descricao} = req?.body;
+            console.log('descricao:', descricao);
 
-            const {descricao, file} = req?.body;
-            
-            if (!descricao || descricao.length < 2) {
-                return res.status(400).json({ erro: 'Descrição não é valida' })
-            
-            };
-            if (!req.file || !req.file.originalname) {
-                return res.status(400).json({ erro: 'imagem é obrigatória' })
-            };
+            if(!descricao || descricao.length < 2){
+                return res.status(400).json({erro : 'Descricao nao e valida'});
+            }
+    
+            if(!req.file || !req.file.originalname){
+                return res.status(400).json({erro : 'Imagem e obrigatória'});
+            }
 
             const image = await uploadImagemCosmic(req);
             const publicacao = {
-                idUsuario: usuario._id,
+                idUsuario : usuario._id,
                 descricao,
-                foto: image.media.url,
-                data: new Date()
+                foto : image.media.url,
+                data : new Date()
             }
 
+        
+
             await PublicacaoModel.create(publicacao);
-
-            return res.status(200).json({ msg: 'Publicação criada com sucesso!' })
-
-        } catch (e) {
+            return res.status(200).json({msg : 'Publicacao criada com sucesso'});
+        }catch(e){
             console.log(e);
-            return res.status(400).json({ erro: 'Erro ao cadastrar publicação' });
+            return res.status(400).json({erro : 'Erro ao cadastrar publicacao'});
         }
-
-    });
-
-
+});
 
 export const config = {
-    api: {
-        bodyParser: false
+    api : {
+        bodyParser : false
     }
 }
 
-export default validarTokenJwt(conectarMongoDB(handler));
+export default validarTokenJwt(conectarMongoDB(handler)); 
--- pages/api/seguir.ts
+++ pages/api/seguir.ts
@@ -1,19 +1,68 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
-import { validarTokenJwt } from "../../middlewares/validarTokenJwt";
-import { conectarMongoDB } from "../../middlewares/conectarMongoDB";
+import type {NextApiRequest, NextApiResponse} from 'next';
+import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
+import { validarTokenJwt } from '../../middlewares/validarTokenJwt';
+import { SeguidorModel } from '../../models/seguidorModel';
+import { UsuarioModel } from '../../models/UsuarioModel';
+import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
 
-const endPointSeguir = ( req: NextApiRequest, res: NextApiResponse<RespostaPadraoMsg>)=>{
-    try {
+const endpointSeguir = 
+    async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
+    try{
+        if(req.method === 'PUT'){
+
+            const {userId, id} = req?.query;
+
+            // usuario logado/autenticado = quem esta fazendo as acoes
+            const usuarioLogado = await UsuarioModel.findById(userId);
+            if(!usuarioLogado){
+                return res.status(400).json({erro : 'Usuario logado nao encontrado'});
+            }
+
+            // id do usuario e ser seguidor - query
+            const usuarioASerSeguido = await UsuarioModel.findById(id);
+            if(!usuarioASerSeguido){
+                return res.status(400).json({ erro : 'Usuario a ser seguido nao encontrado'});
+            }
+
+            // buscar se EU LOGADO sigo ou nao esse usuario
+            const euJaSigoEsseUsuario = await SeguidorModel
+                .find({usuarioId: usuarioLogado._id, usuarioSeguidoId : usuarioASerSeguido._id});
+            if(euJaSigoEsseUsuario && euJaSigoEsseUsuario.length > 0){
+                // sinal que eu ja sigo esse usuario
+                euJaSigoEsseUsuario.forEach(async(e : any) => 
+                    await SeguidorModel.findByIdAndDelete({_id : e._id}));
+                
+                usuarioLogado.seguindo--;
+                await UsuarioModel.findByIdAndUpdate({_id : usuarioLogado._id}, usuarioLogado);
+                usuarioASerSeguido.seguidores--;
+                await UsuarioModel.findByIdAndUpdate({_id : usuarioASerSeguido._id}, usuarioASerSeguido);
+
+                return res.status(200).json({msg : 'Deixou de seguir o usuario com sucesso'});
+            }else{
+                // sinal q eu nao sigo esse usuario
+                const seguidor = {
+                    usuarioId : usuarioLogado._id,
+                    usuarioSeguidoId : usuarioASerSeguido._id
+                };
+                await SeguidorModel.create(seguidor);
+
+                // adicionar um seguindo no usuario logado
+                usuarioLogado.seguindo++;
+                await UsuarioModel.findByIdAndUpdate({_id : usuarioLogado._id}, usuarioLogado);
+
+                // adicionar um seguidor no usuario seguido
+                usuarioASerSeguido.seguidores++;
+                await UsuarioModel.findByIdAndUpdate({_id : usuarioASerSeguido._id}, usuarioASerSeguido);
+
+                return res.status(200).json({msg : 'Usuario seguido com sucesso'});
+            }
+        }
         
-        
-    } catch (e) {
+        return res.status(405).json({erro : 'Método informado nao existe'});
+    }catch(e){
         console.log(e);
-        return res.status(500).json({erro: 'Não foi possível seguir/desseguir o usuario informado'});
-        
+        return res.status(500).json({erro : 'Nao foi possível seguir/desseguir o usuario informado'});
     }
 }
-export default validarTokenJwt(conectarMongoDB(endPointSeguir));
 
-
-
+export default validarTokenJwt(conectarMongoDB(endpointSeguir));
